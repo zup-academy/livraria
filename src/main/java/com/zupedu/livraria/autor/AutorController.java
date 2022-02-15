ad6522cb2158efb2fdaf5063ead5d8abf40f05a8
paula.santana <paula.santana@zup.com.br>
2022-02-15 17:31:39 -0300

    Adicionando ajustes

--- src/main/java/com/zupedu/livraria/autor/AutorController.java
+++ src/main/java/com/zupedu/livraria/autor/AutorController.java
@@ -1,19 +1,20 @@
 package com.zupedu.livraria.autor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/autores")
 public class AutorController {
 
     @Autowired
     private AutorRepository repository;
 
-    @PostMapping
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public AutorResponse incluir(@RequestBody AutorRequest request){
         var autor = repository.save(request.getAutor());
 
--- src/main/java/com/zupedu/livraria/autor/AutorResponse.java
+++ src/main/java/com/zupedu/livraria/autor/AutorResponse.java
@@ -1,5 +1,8 @@
 package com.zupedu.livraria.autor;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
 
 public class AutorResponse {
@@ -10,10 +13,14 @@ public class AutorResponse {
 
     private String email;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime nascimento;
 
     private String biografia;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime inclusao;
 
     private AutorResponse(Long id, String nome, String email,
@@ -31,5 +38,29 @@ public class AutorResponse {
         return new AutorResponse(autor.getId(), autor.getNome(),
                 autor.getEmail(), autor.getDataNascimento(),
                 autor.getBiografia(), autor.getCriado());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDateTime getNascimento() {
+        return nascimento;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public LocalDateTime getInclusao() {
+        return inclusao;
     }
 }
